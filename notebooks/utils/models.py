ea9756306ed9dd5ddf70a5fc0f63385140221e40
ikwak2 <ilyoup.kwak@gmail.com>
2022-07-11 11:28:15 +0900

    update

--- notebooks/utils/models.py
+++ notebooks/utils/models.py
@@ -225,3 +225,170 @@ def get_toy4_2(mel_input_shape):
     
     model.compile(loss=['categorical_crossentropy'], optimizer='adam', metrics=['accuracy'])
     return(model)
+
+
+def get_toy5_1(mel_input_shape, cqt_input_shape, stft_input_shape, use_mel = True, use_cqt = True, use_stft = True):
+        # Create a towy model.
+    age = keras.Input(shape=(6,), name = 'age_cat')
+    sex = keras.Input(shape=(2,), name = 'sex_cat')
+    hw = keras.Input(shape=(2,), name = 'height_weight')
+    preg = keras.Input(shape=(1,), name = 'is_preg')
+    loc = keras.Input(shape=(5,), name = 'loc')
+    mel1 = keras.Input(shape=(mel_input_shape), name = 'mel')
+    cqt1 = keras.Input(shape=(cqt_input_shape), name = 'cqt')
+    stft1 = keras.Input(shape=(stft_input_shape), name = 'stft')
+        
+        
+    ## age embeddig
+    age1 = layers.Dense(2, activation = None)(age)
+
+    ## sex embedding
+    sex1 = layers.Dense(1, activation = None)(sex)
+
+    ## hw embedding
+    hw1 = layers.Dense(1, activation = None)(hw)
+
+    ## loc embedding
+    loc1 = layers.Dense(3, activation = None)(loc)
+
+    ## mel embedding
+    mel2 = layers.Conv2D(16, (3,3), activation = 'relu')(mel1)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.Conv2D(32, (5,5), activation = 'relu')(mel2)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.Conv2D(32, (3,3), activation = 'relu')(mel2)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.Conv2D(64, (3,3), activation = 'relu')(mel2)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.GlobalAveragePooling2D()(mel2)
+
+    ## cqt embedding
+    cqt2 = layers.Conv2D(16, (3,3), activation = 'relu')(cqt1)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.Conv2D(32, (5,5), activation = 'relu')(cqt2)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.Conv2D(32, (3,3), activation = 'relu')(cqt2)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.Conv2D(64, (3,3), activation = 'relu')(cqt2)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.GlobalAveragePooling2D()(cqt2)
+
+    ## stft embedding
+    stft2 = layers.Conv2D(16, (3,3), activation = 'relu')(stft1)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.Conv2D(32, (5,5), activation = 'relu')(stft2)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.Conv2D(32, (3,3), activation = 'relu')(stft2)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.Conv2D(64, (3,3), activation = 'relu')(stft2)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.GlobalAveragePooling2D()(stft2)
+    
+#    concat1 = layers.Concatenate()([age1, sex1, hw1, loc1, preg])
+#    d1 = layers.Dense(2, activation = 'relu')(concat1)
+    
+    if use_mel and use_cqt and use_stft :
+        concat2 = layers.Concatenate()([mel2, cqt2, stft2])
+    if not use_mel and use_cqt and use_stft :
+        concat2 = layers.Concatenate()([cqt2, stft2])
+    if use_mel and not use_cqt and use_stft :
+        concat2 = layers.Concatenate()([mel2, stft2])
+    if use_mel and use_cqt and not use_stft :
+        concat2 = layers.Concatenate()([mel2, cqt2])
+    if not use_mel and not use_cqt and use_stft :  ## stft 만
+        concat2 = layers.Concatenate()([stft2])
+    if use_mel and not use_cqt and not use_stft :  ### mel만
+        concat2 = layers.Concatenate()([mel2])
+    if not use_mel and use_cqt and not use_stft :  ### cqt만
+        concat2 = layers.Concatenate()([cqt2])
+#    concat2 = layers.Dense(10, activation = 'relu')(concat2)
+    res1 = layers.Dense(3, activation = "softmax")(concat2)
+    res2 = layers.Dense(2, activation = "softmax")(concat2)
+
+    model = keras.Model(inputs = [age,sex,hw,preg,loc,mel1,cqt1,stft1] , outputs = res1 )
+    
+    model.compile(loss=['categorical_crossentropy'], optimizer='adam', metrics=['accuracy'])
+    return(model)
+
+def get_toy5_2(mel_input_shape, cqt_input_shape, stft_input_shape, use_mel = True, use_cqt = True, use_stft = True):
+        # Create a towy model.
+    age = keras.Input(shape=(6,), name = 'age_cat')
+    sex = keras.Input(shape=(2,), name = 'sex_cat')
+    hw = keras.Input(shape=(2,), name = 'height_weight')
+    preg = keras.Input(shape=(1,), name = 'is_preg')
+    loc = keras.Input(shape=(5,), name = 'loc')
+    mel1 = keras.Input(shape=(mel_input_shape), name = 'mel')
+    cqt1 = keras.Input(shape=(cqt_input_shape), name = 'cqt')
+    stft1 = keras.Input(shape=(stft_input_shape), name = 'stft')
+        
+        
+    ## age embeddig
+    age1 = layers.Dense(2, activation = None)(age)
+
+    ## sex embedding
+    sex1 = layers.Dense(1, activation = None)(sex)
+
+    ## hw embedding
+    hw1 = layers.Dense(1, activation = None)(hw)
+
+    ## loc embedding
+    loc1 = layers.Dense(3, activation = None)(loc)
+
+    ## mel embedding
+    mel2 = layers.Conv2D(16, (3,3), activation = 'relu')(mel1)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.Conv2D(32, (5,5), activation = 'relu')(mel2)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.Conv2D(32, (3,3), activation = 'relu')(mel2)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.Conv2D(64, (3,3), activation = 'relu')(mel2)
+    mel2 = layers.MaxPooling2D()(mel2)
+    mel2 = layers.GlobalAveragePooling2D()(mel2)
+
+    ## cqt embedding
+    cqt2 = layers.Conv2D(16, (3,3), activation = 'relu')(cqt1)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.Conv2D(32, (5,5), activation = 'relu')(cqt2)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.Conv2D(32, (3,3), activation = 'relu')(cqt2)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.Conv2D(64, (3,3), activation = 'relu')(cqt2)
+    cqt2 = layers.MaxPooling2D()(cqt2)
+    cqt2 = layers.GlobalAveragePooling2D()(cqt2)
+
+    ## stft embedding
+    stft2 = layers.Conv2D(16, (3,3), activation = 'relu')(stft1)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.Conv2D(32, (5,5), activation = 'relu')(stft2)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.Conv2D(32, (3,3), activation = 'relu')(stft2)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.Conv2D(64, (3,3), activation = 'relu')(stft2)
+    stft2 = layers.MaxPooling2D()(stft2)
+    stft2 = layers.GlobalAveragePooling2D()(stft2)
+    
+#    concat1 = layers.Concatenate()([age1, sex1, hw1, loc1, preg])
+#    d1 = layers.Dense(2, activation = 'relu')(concat1)
+    
+    if use_mel and use_cqt and use_stft :
+        concat2 = layers.Concatenate()([mel2, cqt2, stft2])
+    if not use_mel and use_cqt and use_stft :
+        concat2 = layers.Concatenate()([cqt2, stft2])
+    if use_mel and not use_cqt and use_stft :
+        concat2 = layers.Concatenate()([mel2, stft2])
+    if use_mel and use_cqt and not use_stft :
+        concat2 = layers.Concatenate()([mel2, cqt2])
+    if not use_mel and not use_cqt and use_stft :  ## stft만
+        concat2 = layers.Concatenate()([stft2])
+    if use_mel and not use_cqt and not use_stft :  ### mel만
+        concat2 = layers.Concatenate()([mel2])
+    if not use_mel and use_cqt and not use_stft :  ### cqt만
+        concat2 = layers.Concatenate()([cqt2])
+#    concat2 = layers.Dense(10, activation = 'relu')(concat2)
+    res1 = layers.Dense(3, activation = "softmax")(concat2)
+    res2 = layers.Dense(2, activation = "softmax")(concat2)
+
+    model = keras.Model(inputs = [age,sex,hw,preg,loc,mel1,cqt1,stft1] , outputs = res2 )
+    
+    model.compile(loss=['categorical_crossentropy'], optimizer='adam', metrics=['accuracy'])
+    return(model)
