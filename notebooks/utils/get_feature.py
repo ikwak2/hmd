9d38342275a58bed0e0db22e53eaa310bef60e04
ikwak2 <ilyoup.kwak@gmail.com>
2022-07-26 21:25:48 +0900

    bug fix

--- notebooks/utils/get_feature.py
+++ notebooks/utils/get_feature.py
@@ -4,14 +4,23 @@ import librosa
 from helper_code import *
 import math
 import os, numpy as np, scipy as sp, scipy.io, scipy.io.wavfile
+import neurokit2 as nk
+import tqdm
 
-def feature_extract_melspec(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, hop_length=256, win_length = 512, n_mels = 100, trim = 4000):
+
+def feature_extract_melspec(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, hop_length=256, win_length = 512, n_mels = 100, trim = 0):
 
     if isinstance(fnm, str) :
         data, sample_rate = librosa.load(fnm, sr = sr)
-        data = data[trim:-trim] * 1.0
+        if trim :
+            data = data[trim:-trim] * 1.0
+        else :
+            data = data * 1.0
     else :
-        data = fnm[trim:-trim] * 1.0
+        if trim :
+            data = fnm[trim:-trim] * 1.0
+        else :
+            data = fnm * 1.0
         sample_rate = sr
 
     if samp_sec:
@@ -49,13 +58,19 @@ def feature_extract_melspec(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, hop_l
 
 
 
-def feature_extract_stft(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, hop_length=256, win_length = 512, trim = 4000):
+def feature_extract_stft(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, hop_length=256, win_length = 512, trim = 0):
 
     if isinstance(fnm, str) :
         data, sample_rate = librosa.load(fnm, sr = sr)
-        data = data[trim:-trim] * 1.0
+        if trim :
+            data = data[trim:-trim] * 1.0
+        else :
+            data = data * 1.0
     else :
-        data = fnm[trim:-trim] * 1.0
+        if trim :
+            data = fnm[trim:-trim] * 1.0
+        else :
+            data = fnm * 1.0
         sample_rate = sr
 
     if samp_sec:
@@ -91,13 +106,19 @@ def feature_extract_stft(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, hop_leng
     return Sig
 
 
-def feature_extract_cqt(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, filter_scale = 1, n_bins = 80, fmin = 10, trim = 4000):
+def feature_extract_cqt(fnm, samp_sec=20, sr = 4000, pre_emphasis = 0, filter_scale = 1, n_bins = 80, fmin = 10, trim = 0):
 
     if isinstance(fnm, str) :
         data, sample_rate = sf.read(fnm, dtype = 'int16')
-        data = data[trim:-trim] * 1.0
+        if trim :
+            data = data[trim:-trim] * 1.0
+        else :
+            data = data * 1.0
     else :
-        data = fnm[trim:-trim] * 1.0
+        if trim :
+            data = fnm[trim:-trim] * 1.0
+        else :
+            data = fnm * 1.0
         sample_rate = sr
 
 
@@ -1114,3 +1135,211 @@ def get_features_3lb_all(data_folder, patient_files_trn,
     mm_labels = np.array(mm_labels)
     out_labels = np.array(out_labels)
     return features, mm_labels, out_labels, mel_input_shape, cqt_input_shape, stft_input_shape
+
+
+
+def get_features_3lb_all_jk(data_folder, patient_files_trn, 
+                          samp_sec=20, pre_emphasis = 0, hop_length=256, win_length = 512, n_mels = 100,
+                          filter_scale = 1, n_bins = 80, fmin = 10, trim = 4000,
+                         use_mel = True, use_cqt = False, use_stft= False, use_raw = False,use_rr = False
+                         ) :
+    features = dict()
+    features['id'] = []
+    features['age'] = []
+    features['sex'] = []
+    features['hw'] = []
+    features['preg'] = []
+    features['loc'] = []
+    features['mel1'] = []
+    features['cqt1'] = []
+    features['stft1'] = []
+    features['raw1'] = []
+    features['rr1'] = []
+
+    mm_labels = []
+    out_labels = []
+
+    age_classes = ['Neonate', 'Infant', 'Child', 'Adolescent', 'Young Adult']
+    recording_locations = ['AV', 'MV', 'PV', 'TV', 'PhC']
+
+    num_patient_files = len(patient_files_trn)
+
+    for i in tqdm.tqdm(range(num_patient_files)):
+
+        # Load the current patient data and recordings.
+        current_patient_data = load_patient_data(patient_files_trn[i])
+        num_locations = get_num_locations(current_patient_data)
+        recording_information = current_patient_data.split('\n')[1:num_locations+1]
+        for j in range(num_locations) :
+            entries = recording_information[j].split(' ')
+            recording_file = entries[2]
+            filename = os.path.join(data_folder, recording_file)
+
+            # Extract id
+            id1 = recording_file.split('_')[0]
+            features['id'].append(id1)
+
+            # Extract melspec
+            if use_mel :
+                mel1 = feature_extract_melspec(filename, samp_sec=samp_sec, pre_emphasis = pre_emphasis, hop_length=hop_length, 
+                                               win_length = win_length, n_mels = n_mels, trim = trim)[0]
+            else :
+                mel1 = np.zeros( (1,1,1) )
+            features['mel1'].append(mel1)
+
+            if use_cqt :
+                mel2 = feature_extract_cqt(filename, samp_sec=samp_sec, pre_emphasis = pre_emphasis, filter_scale = filter_scale, 
+                                           n_bins = n_bins, fmin = fmin, trim = trim)[0]
+            else :
+                mel2 = np.zeros( (1,1,1) )                
+            features['cqt1'].append(mel2)
+
+            if use_stft :
+                mel3 = feature_extract_stft(filename, samp_sec=samp_sec, pre_emphasis = pre_emphasis, hop_length=hop_length, 
+                                            win_length = win_length, trim = trim)[0]
+            else :
+                mel3 = np.zeros( (1,1,1) )
+            features['stft1'].append(mel3)
+
+            if use_raw :
+                frequency1, recording1 = sp.io.wavfile.read(filename)
+            else :
+                recording1 = np.zeros( (1) )
+            features['raw1'].append(recording1)
+            
+            if use_rr :
+                try:
+                    ____, recording1 = sp.io.wavfile.read(filename)
+                    ____, info = nk.ecg_process(recording1, sampling_rate=4000)
+                    rr = np.mean(np.diff(info['ECG_R_Peaks'])/4000)
+                    if rr>1:
+                        current_rr = np.array([0,0,1])
+                    elif rr<0.6:
+                        current_rr = np.array([1,0,0])
+                    else :
+                        current_rr = np.array([0,1,0])
+                except:
+                    print(filename)
+                    current_rr=np.array([0,0,0])
+            else :
+                
+                current_rr = np.zeros((1))
+                
+            features['rr1'].append(current_rr)            
+            
+            # Extract age_group
+            age_group = get_age(current_patient_data)
+            current_age_group = np.zeros(6, dtype=int)
+            if age_group in age_classes:
+                j = age_classes.index(age_group)
+                current_age_group[j] = 1
+            else :
+                current_age_group[5] = 1
+            features['age'].append(current_age_group)
+
+            # Extract sex
+            sex = get_sex(current_patient_data)
+            sex_features = np.zeros(2, dtype=int)
+            if compare_strings(sex, 'Female'):
+                sex_features[0] = 1
+            elif compare_strings(sex, 'Male'):
+                sex_features[1] = 1
+            features['sex'].append(sex_features)
+
+            # Extract height and weight.
+            height = get_height(current_patient_data)
+            weight = get_weight(current_patient_data)
+            ## simple impute
+            if math.isnan(height) :
+                height = 110.846
+            if math.isnan(weight) :
+                weight = 23.767
+                
+            features['hw'].append(np.array([height, weight]))
+
+            # Extract pregnancy
+            is_pregnant = get_pregnancy_status(current_patient_data)
+            features['preg'].append(is_pregnant)
+
+            # Extract location
+            locations = entries[0]
+            num_recording_locations = len(recording_locations)
+            loc_features = np.zeros(num_recording_locations)
+            if locations in recording_locations:
+                j = recording_locations.index(locations)
+                loc_features[j] = 1
+            features['loc'].append(loc_features)
+
+            # Extract labels 
+            mm_label = get_murmur(current_patient_data)
+            out_label = get_outcome(current_patient_data)
+            current_mm_labels = np.zeros(2)
+            current_out_labels = np.zeros(2)
+            if mm_label == 'Absent' :
+                current_mm_labels = np.array([0, 0, 1])
+            elif mm_label == 'unknown' :
+                current_mm_labels = np.array([0, 1, 0])
+            else :
+                mm_loc = get_murmur_loc(current_patient_data)
+                if mm_loc == 'nan' :
+                    current_mm_labels = np.array([0.9, 0.05, 0.05])
+                else :
+                    mm_loc = mm_loc.split('+')
+                    if locations in mm_loc :
+                        current_mm_labels = np.array([1, 0, 0])
+                    else :
+                        current_mm_labels = np.array([0.7, 0.2, 0.1])
+
+            if out_label == 'Normal' :
+                current_out_labels = np.array([0, 1])
+            else :
+                current_out_labels = np.array([1, 0])
+#                if mm_label == 'Absent' :
+#                    current_out_labels = np.array([0.8, 0.2])
+#                elif mm_label == 'unknown' :
+#                    current_out_labels = np.array([0.85, 0.15])
+#                else :
+#                    current_out_labels = np.array([1, 0])
+                
+            mm_labels.append(current_mm_labels)
+            out_labels.append(current_out_labels)
+
+    if use_mel : 
+        M, N = features['mel1'][i].shape
+        for i in range(len(features['mel1'])) :
+            features['mel1'][i] = features['mel1'][i].reshape(M,N,1)
+        print("melspec: ", M,N)
+    else :
+        M, N, _ = features['mel1'][i].shape
+    mel_input_shape = (M,N,1)
+        
+    if use_cqt :
+        M, N = features['cqt1'][i].shape
+        for i in range(len(features['cqt1'])) :
+            features['cqt1'][i] = features['cqt1'][i].reshape(M,N,1)
+        print("cqt: ", M,N)
+    else :
+        M, N, _ = features['cqt1'][i].shape
+    cqt_input_shape = (M,N,1)
+
+    
+    if use_stft :
+        M, N = features['stft1'][i].shape
+        for i in range(len(features['stft1'])) :
+            features['stft1'][i] = features['stft1'][i].reshape(M,N,1)
+        print("stft: ", M,N)
+    else :
+        M, N, _ = features['stft1'][i].shape
+    stft_input_shape = (M,N,1)
+    
+
+     
+    for k1 in features.keys() :
+        features[k1] = np.array(features[k1])    
+    
+    mm_labels = np.array(mm_labels)
+    out_labels = np.array(out_labels)
+    
+    return features, mm_labels, out_labels, mel_input_shape, cqt_input_shape, stft_input_shape
+
+
